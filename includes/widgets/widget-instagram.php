c35f57163fc742ae838a52d4be7507c551db7959
Ram Ratan Maurya <ratan.maurya@gmail.com>
2016-02-18 17:28:00 +0530

    Update Instagram widget to work with new API

--- includes/widgets/widget-instagram.php
+++ includes/widgets/widget-instagram.php
@@ -25,26 +25,6 @@ class Stag_Instagram extends ST_Widget {
 				'placeholder' => 'myusername',
 				'label'       => __( 'Instagram Username:', 'stag' ),
 			),
-			'user_id' => array(
-				'type'        => 'text',
-				'std'         => null,
-				'placeholder' => '476220644',
-				'label'       => __( 'User ID:', 'stag' ),
-			),
-			'user_id_desc' => array(
-				'type' => 'description',
-				'std'  => sprintf( __( 'Lookup your User ID <a href="%1$s" target="_blank">here</a>', 'stag' ), 'http://jelled.com/instagram/lookup-user-id' )
-			),
-			'client_id' => array(
-				'type'        => 'text',
-				'std'         => null,
-				'placeholder' => '943c89932b2a47e6ae341d3d1943e73f',
-				'label'       => __( 'Client ID:', 'stag' ),
-			),
-			'client_id_desc' => array(
-				'type' => 'description',
-				'std'  => sprintf( __( 'Register a new client <a href="%1$s" target="_blank">here</a>', 'stag' ), 'http://instagram.com/developer/clients/manage/' )
-			),
 			'count' => array(
 				'type'  => 'number',
 				'std'   => 9,
@@ -70,7 +50,17 @@ class Stag_Instagram extends ST_Widget {
 				'step'  => 1,
 				'min'   => 1,
 				'max'   => 500,
-			)
+			),
+			'follow_link_show' => array(
+				'type'  => 'checkbox',
+				'std'   => false,
+				'label' => __( 'Include link to Instagram page?', 'stag' ),
+			),
+			'follow_link_text' => array(
+				'type'  => 'text',
+				'std'   => 'Follow on Instagram',
+				'label' => __( 'Link Text:', 'stag' ),
+			),
 		);
 
 		parent::__construct();
@@ -96,16 +86,13 @@ class Stag_Instagram extends ST_Widget {
 
 		$title     = apply_filters( 'widget_title', $instance['title'] );
 		$username  = esc_html( $instance['username'] );
-		$user_id   = absint( $instance['user_id'] );
-		$client_id = esc_html( $instance['client_id'] );
 		$count     = absint( $instance['count'] );
 		$image_res = esc_html( $instance['size'] );
 		$cachetime = absint( $instance['cachetime'] );
 
 		// Get Instagrams
 		$instagram = $this->get_instagrams( array(
-			'user_id'   => $user_id,
-			'client_id' => $client_id,
+			'username'  => $username,
 			'count'     => $count,
 			'cachetime' => $cachetime,
 		) );
@@ -119,18 +106,26 @@ class Stag_Instagram extends ST_Widget {
 
 			<ul class="instagram-widget <?php echo esc_attr( $image_res ); ?>">
 				<?php
-					foreach ( $instagram['data'] as $key => $image ) {
-						echo apply_filters( 'st_instagram_widget_image_html', sprintf( '<li><a href="%1$s"><img class="instagram-image" src="%2$s" alt="%3$s" title="%3$s" /></a></li>',
-							$image['link'],
-							str_replace( 'http:', '', $image['images'][ $image_res ]['url'] ),
-							$image['caption']['text']
-						), $image );
+					$displayed = 0;
+					foreach ( $instagram['items'] as $key => $image ) {
+						$displayed++;
+
+						if ( $displayed <= $count ) {
+							echo apply_filters( 'st_instagram_widget_image_html', sprintf( '<li><a href="%1$s"><img class="instagram-image" src="%2$s" alt="%3$s" title="%3$s" /></a></li>',
+								$image['link'],
+								$image['images'][ $image_res ]['url'],
+								$image['caption']['text']
+							), $image );
+						}
+
 					}
 				?>
 
 			</ul>
 
-			<a class="instagram-follow-link" href="https://instagram.com/<?php echo esc_html( $username ); ?>"><?php printf( __( 'Follow %1$s on Instagram', 'stag' ), esc_html( $username ) ); ?></a>
+			<?php if ( $instance['follow_link_show'] && $instance['follow_link_text'] ) : ?>
+			<a class="stag-button instagram-follow-link" href="https://instagram.com/<?php echo esc_html( $username ); ?>"><?php echo esc_html( $instance['follow_link_text'] ); ?></a>
+			<?php endif; ?>
 
 		<?php elseif ( ( defined( 'WP_DEBUG' ) && true === WP_DEBUG ) && ( defined( 'WP_DEBUG_DISPLAY' ) && false !== WP_DEBUG_DISPLAY ) ) : ?>
 			<div id="message" class="error"><p><?php _e( 'Error: We were unable to fetch your instagram feed.', 'stag' ); ?></p></div>
@@ -153,25 +148,21 @@ class Stag_Instagram extends ST_Widget {
 	 */
 	public function get_instagrams( $args = array() ) {
 		// Get args
-		$user_id   = ( ! empty( $args['user_id'] ) ) ? $args['user_id'] : '';
-		$client_id = ( ! empty( $args['client_id'] ) ) ? $args['client_id'] : '';
+		$username   = ( ! empty( $args['username'] ) ) ? $args['username'] : '';
 		$count     = ( ! empty( $args['count'] ) ) ? $args['count'] : 9;
 		$cachetime = ( ! empty( $args['cachetime'] ) ) ? $args['cachetime'] : 2;
 
-		// If no client id or user id, bail
-		if ( empty( $client_id ) || empty( $user_id ) ) {
+		// If no user id, bail
+		if ( empty( $username ) ) {
 			return false;
 		}
 
-		$key = 'st_instagram_widget_' . $user_id;
+		$key = "stag_instagram_{$username}";
 
 		if ( false === ( $instagrams = get_transient( $key ) ) ) {
 			// Ping Instagram's API
-			$api_url = 'https://api.instagram.com/v1/users/' . esc_html( $user_id ) . '/media/recent/';
-			$response = wp_remote_get( add_query_arg( array(
-				'client_id' => esc_html( $client_id ),
-				'count'     => absint( $count )
-			), $api_url ) );
+			$api_url = "https://www.instagram.com/{$username}/media/";
+			$response = wp_remote_get( $api_url );
 
 			// Check if the API is up.
 			if ( ! 200 == wp_remote_retrieve_response_code( $response ) ) {
